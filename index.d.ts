8edb81d7d5b8c2180a23606fb2f2b60e0c38f407
Ronis <60937516+xogumon@users.noreply.github.com>
2022-07-09 21:20:33 -0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,24 +2,24 @@
 // Project: session-xp
 // Definitions by: Ronis Xogum <https://github.com/xogumon>
 declare module "session-xp" {
-    export = sessionXp
-    class sessionXp {
-        constructor (options?: sessionXp.options)
-    }
+  export = sessionXp;
+  class sessionXp {
+    constructor(options?: sessionXp.options);
+  }
 }
 declare namespace sessionXp {
-    export interface options {
-        collection?: string,
-        dbname?: string,
-        expires?: expiresObject
-    }
-    export interface expiresObject {
-        years?: number,
-        months?: number,
-        weeks?: number,
-        days?: number,
-        hours?: number,
-        minutes?: number,
-        seconds?: number
-    }
+  export interface options {
+    collection?: string;
+    dbname?: string;
+    expires?: expiresObject;
+  }
+  export interface expiresObject {
+    years?: number;
+    months?: number;
+    weeks?: number;
+    days?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+  }
 }
